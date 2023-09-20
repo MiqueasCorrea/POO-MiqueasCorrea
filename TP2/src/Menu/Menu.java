2f4224695bbd84cc5b230b7e838225390e52e2ff
MiqueasCorrea <mikeuni2004@gmail.com>
2023-09-19 22:09:20 -0300

    added generic function for create menus

---
+++ TP2/src/Menu/Menu.java
@@ -0,0 +1,46 @@
+package Menu;
+import java.util.List;
+import java.util.Scanner;
+public class Menu {
+    // Funcion generica porque me canse de crear menus a mano, saracatunga
+    // le paso los parametros: titulo, y una lista de opciones
+
+    /**
+     * Funcion generica para crear un menu
+     * @param titulo
+     * @param opciones
+     * @autor DonChoton (miqueas)
+     */
+    public static int mostrarMenu(String titulo, List<String> opciones){
+        Scanner escaner = new Scanner(System.in);
+
+        boolean fin = false;
+
+        do {
+            System.out.println("\t\t" + AnsiColorsitos.YELLOW + titulo + AnsiColorsitos.RESET);
+
+            int numero_de_opcion = 1;
+            for (String i : opciones){
+                System.out.println(AnsiColorsitos.YELLOW + numero_de_opcion + AnsiColorsitos.RESET + ". " + i);
+                numero_de_opcion++;
+            }
+
+            System.out.println(AnsiColorsitos.YELLOW + "0" + AnsiColorsitos.RESET + ". Salir");
+            System.out.print(AnsiColorsitos.YELLOW + "Ingrese una opcion: " + AnsiColorsitos.RESET);
+            try {
+                int respuesta = escaner.nextInt();
+                if (respuesta >= 0 && respuesta <= opciones.size()){
+                    if(respuesta == 0){
+                        fin = true;
+                        System.out.println(AnsiColorsitos.YELLOW + "Saliendo del programa..." + AnsiColorsitos.RESET);
+                    }
+                    return respuesta;
+                }
+            } catch (Exception e){
+                System.err.println("Error, ingrese un numero.");
+                escaner.nextLine();
+            }
+        } while(!fin);
+        return 0; // se activa si ingresa 0
+    }
+}
---
+++ TP2/src/P2/Punto2.java
@@ -0,0 +1,10 @@
+package P2;
+
+import Menu.Menu;
+import java.util.List;
+
+public class Punto2 {
+    public static void main(String[] args){
+        Menu.mostrarMenu("Pepe", List.of("Saracatunga", "Sorongol", "Pepingo", "Tatito"));
+    }
+}
