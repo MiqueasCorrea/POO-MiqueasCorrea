ebf4b2e81eb97d2cfec78dc89c12a2a55d63f684
MiqueasCorrea <mikeuni2004@gmail.com>
2023-09-19 23:06:59 -0300

    test

--- TP2/src/P2/Punto2.java
+++ TP2/src/P2/Punto2.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class Punto2 {
     public static void main(String[] args){
-        Menu.mostrarMenu("Pepe", List.of("Saracatunga", "Sorongol", "Pepingo", "Tatito"));
+        Menu.mostrarMenu("Pepe", List.of("Saracatunga", "Sorongol", "Pepingo", "Tatito", "pepito 22", "july"));
     }
 }
